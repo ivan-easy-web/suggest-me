8ed0b888cae3bcf650dfad0af6717d3d9bf3248f
username-HelloKitty <60348590+username-HelloKitty@users.noreply.github.com>
2020-11-28 16:47:41 +0300

    implement playlist suggestion

--- music.js
+++ music.js
@@ -1,18 +1,51 @@
 const musicIntersts = [];
+const playlist = [];
+var lastSearch;
 
-musicIntersts.handle = function() {
-  
+musicIntersts.add = function(trackId){
+  for (let track of musicIntersts) {
+    if (track.trackId == trackId){
+      return;
+    }
+  }
+  for (let track of lastSearch.results) {
+    if (track.trackId == trackId) {
+      musicIntersts.push(track);
+      let text = `${track.trackName} - ${track.artistName}`;
+      ($(`<li class="list-group-item d-flex justify-content-between align-items-center" trackId="${track.trackId}">${text}<span class="badge badge-outline-dark badge-pill">-</span></li>`))
+      .appendTo($('#interests-list'));
+    }
+  };
 }
 
-musicIntersts.render = function() {
-  const $list = $('.info').eq(0);
+musicIntersts.handle = function() {
+  if (musicIntersts.length < 5) {
+    // to do 
+  } else {
+    let IdList = [];
+    for (let track of musicIntersts) {
+      if (!IdList.includes(track.artistId)) {
+        IdList.push(track.artistId);
+      }
+    }
+    let url = `https://itunes.apple.com/lookup?id=${IdList}&entity=song&limit=5&sort=recent&callback=initializePlaylist`;
+    $(`<script src="${url}"/>`).appendTo('#scripts');
+  }
+}
 
-  $list.empty();
+function initializePlaylist(response) {
 
-  for (const song of musicIntersts) {
-    const $item = $('<li class="list-group-item">').text(info);
-
-    $list.append($item)
+  if (response.resultCount > 0) {
+    for (let result of response.results) {
+      if (result.wrapperType == 'track'){
+        playlist.push(result);
+        let text = `${result.trackName} - ${result.artistName}`;
+        $(`<li class="list-group-item d-flex justify-content-between align-items-center" trackId="${result.trackId}">${text}<span class="badge badge-outline-dark badge-pill">Play</span></li>`)
+        .appendTo('#playlist');
+      }
+      
+    }
+    $('#myTab a[href="#playlist-page"]').tab('show')
   }
 }
 
@@ -20,43 +53,35 @@ musicIntersts.clear = function() {
   musicIntersts.length = 0;
 }
 
-function get(url) {
-  return new Promise(function(resolve, reject){
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.send();
-    request.onload(function() {
-      if (request.status == 200) {
-        resolve(JSON.parse(request.response));
-      } else {
-        reject(request.statusText);
-      }
-    })
-    request.onerror(function() {
-      reject(request.statusText);
-    })
-  })
-}
-
-$('#searchButton').click(function() {
-  let text = $('#searchField').text();
-  let url = `https://itunes.apple.com/search?media=music&term=${text}`;
-  get(url).then(results => {
-    for(result of results) {
-      text = `Artist name: ${result.artistName}, Song name: ${result.trackName}`;
-      $(`<li>${text}</li>`).appendTo('#suggestions-list');
-      $('#suggestions-list').style('display', 'block');
-    }
-  })
-})
 
 $('#searchField').keyup(function(event) {
-  if (event.which == 13) { // User presses Enter
+  if (event.which == 13) {
     $('#searchButton').click();
   }
 });
 
-$('#getPlaylistBtn').click(function (event) {
+$('#searchButton').click(function() {
+  let text = $('#searchField').val();
+  let url = `https://itunes.apple.com/search?media=music&term=${text}&callback=initializeSuggestionsList`;
+  $(`<script src="${url}"/>`).appendTo('#scripts');
+})
 
-  // todo
+function initializeSuggestionsList(response) {
+  lastSearch = response;
+  let suggestionsList = $('#suggestions-list');
+  suggestionsList.empty();
+  if (response.resultCount > 0) {
+    for (let result of response.results) {
+      let text = `${result.trackName} - ${result.artistName}`;
+      $(`<li class="list-group-item d-flex justify-content-between align-items-center" trackId="${result.trackId}">${text}<span class="badge badge-outline-dark badge-pill">+</span></li>`)
+      .click( function() {
+        musicIntersts.add($(this).attr('trackId'));
+      }).appendTo(suggestionsList);
+    }
+  }
+}
+
+
+$('#getPlaylistBtn').click(function () {
+  musicIntersts.handle();
 });
